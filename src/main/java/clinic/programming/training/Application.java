79a28c2ce1bc4b07abdce065fa904692d253bded
Kento75 <kento2github@gmail.com>
2019-08-10 11:11:29 +0900

    Fix compiler plugin to java source ver1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,18 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+
+        for (String greeting : greetings) {
+            System.out.println("Greeting: " + greeting);
+        }
+    }
 
     public Application() {
         System.out.println("Inside Application");
@@ -10,5 +22,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Starting Application");
         Application app = new Application();
+        app.greet();
     }
 }
